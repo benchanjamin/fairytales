9985aaed5ab8f8060a42b87ac3edcb99daa0c251
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-10-27 17:48:42 -0400

    added new images + caption on image hover + less padding for nav bar + more padding for hero text

--- components/BetterHeader.tsx
+++ components/BetterHeader.tsx
@@ -3,14 +3,14 @@ import {NavLink} from "@components/NavLink";
 export default function BetterHeader() {
     return (
         <>
-            <nav className="container relative mx-auto pt-6 h-[15vh]">
+            <nav className="container relative mx-auto pt-6 h-[10vh]">
                 <div className="flex-col items-start">
                     <div>
                         <h2 className="text-[14px] text-white text-left font-bold font-light pl-6 pr-6">FAIRY WEB</h2>
                     </div>
 
                     <div className="hidden items-center justify-between space-x-30 md:flex text-[28px]
-                    mt-[29px] pl-6 pr-6">
+                    mt-[15px] pl-6 pr-6">
                         <NavLink href="/" exact className="text-white italic font-medium hover:text-lightBlue duration-75">Home</NavLink>
                         <NavLink href="/timeline" exact className="text-white italic font-medium hover:text-lightBlue duration-75">Timeline</NavLink>
                         <NavLink href="/map" exact className="text-white italic font-medium hover:text-lightBlue duration-75">Map</NavLink>
--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -7,7 +7,7 @@ function HomeHero(props) {
         <section id="hero">
             <div className="container mx-auto relative">
                 {/* Content Words */}
-                <div className="flex justify-center h-[15vh] items-center mx-auto w-[55%] md:h-[30vh] 3xl:w-[80%]">
+                <div className="flex justify-center h-[15vh] items-center mx-auto w-[55%] md:h-[35vh] 3xl:w-[80%]">
                     <h1 className="text-white text-center font-medium text-md md:text-[28px] 3xl:text-[50px]">
                         Fairy Web is a digital humanities project that includes information and
                         data visualizations about the 19th century British fairy tale.
@@ -16,17 +16,46 @@ function HomeHero(props) {
             </div>
             {/* Content Images */}
             <div className="flex flex-col md:flex-row">
-                <div className="w-full md:w-1/3 relative h-[55vh]">
+                <div className="w-full md:w-1/3 relative h-[55vh] group">
                     <Image layout={"fill"} objectFit={"cover"} objectPosition={"50% 35%"} alt={"fairytale"}
                            src="/static/image1.jpg" placeholder="blur" blurDataURL="/static/image1.jpg"/>
+                    <div className="absolute top-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-gray-700
+                     opacity-0 group-hover:opacity-100 bg-opacity-90">
+                        <div className="flex justify-center w-full">
+                            <div className="font-normal">
+                                <p className="text-sm">Illustration by Richard Doyle for John Ruskin’s The King of the
+                                    Golden River (from the first edition, published by Smith, Elder & Co. in 1851)</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="w-full md:w-1/3 relative h-[55vh]">
+                <div className="w-full md:w-1/3 relative h-[55vh] group">
                     <Image layout={"fill"} objectFit={"cover"} objectPosition={"50% 20%"} alt={"fairytale"}
                            src="/static/image2.jpg" placeholder="blur" blurDataURL="/static/image2.jpg"/>
+                    <div className="absolute top-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-gray-700
+                     opacity-0 group-hover:opacity-100 bg-opacity-90">
+                        <div className="flex justify-center w-full">
+                            <div className="font-normal">
+                                <p className="text-sm">Illustration by Charles Ricketts & Charles Shannon for Oscar
+                                    Wilde’s A House of Pomegranates (from the first edition, published by James R.
+                                    Osgoode McIlvaine, 1891)</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="w-full md:w-1/3 relative h-[55vh]">
+                <div className="w-full md:w-1/3 relative h-[55vh] group">
                     <Image layout={"fill"} objectFit={"cover"} objectPosition={"50% 0%"} alt={"fairytale"}
                            src="/static/image3.jpg" placeholder="blur" blurDataURL="/static/image3.jpg"/>
+                    <div className="absolute top-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-gray-700
+                     opacity-0 group-hover:opacity-100 bg-opacity-90">
+                        <div className="flex justify-center w-full">
+                            <div className="font-normal">
+                                <p className="text-sm">Illustration by Walter Crane for Mary de Morgan’s The Necklace of
+                                    Princess Fiorimonde and Other Stories (from the first edition, published by
+                                    Macmillan & Co. in 1880)</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
