4337ced7f9dfc131dbe98fb2f8efc178d09d61be
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-01-24 19:41:23 -0500

    stick to translate from left for intersection observer

--- public/static/timeline.js
+++ public/static/timeline.js
@@ -8,6 +8,18 @@ const observer = new IntersectionObserver((entries) => {
     rootMargin: '0px',
     threshold: .1,
 })
+// const observerRight = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.remove('hidden_item_right')
+//         }
+//     })
+// }, {
+//     rootMargin: '0px',
+//     threshold: .1,
+// })
 
 const hiddenElements = document.querySelectorAll('.hidden_item')
+// const hiddenElementsRight = document.querySelectorAll('.hidden_item_right')
 hiddenElements.forEach((elem) => observer.observe(elem))
+// hiddenElementsRight.forEach((elem) => observerRight.observe(elem))
