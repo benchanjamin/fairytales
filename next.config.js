c516149603319a6d3867734165ee44e07ad0f8fe
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-01-24 19:47:28 -0500

    make images optimized

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  images: {
+    formats: ['image/webp'],
+  },
 }
 
 module.exports = nextConfig
