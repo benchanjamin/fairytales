17f374e394012781488243f74598f7f99fc1d06f
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-03-09 17:51:40 -0500

    add image attributions

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Image from "next/image";
+import Link from "next/link";
 
 
-const imageKitLoader = ({ src, width, quality }) => {
-    if(src[0] === "/") src = src.slice(1);
+const imageKitLoader = ({src, width, quality}) => {
+    if (src[0] === "/") src = src.slice(1);
     const params = [`w-${width}`];
     if (quality) {
         params.push(`q-${quality}`);
     }
     const paramsString = params.join(",");
     let urlEndpoint = "https://ik.imagekit.io/mezdticvf";
-    if(urlEndpoint[urlEndpoint.length-1] === "/") urlEndpoint = urlEndpoint.substring(0, urlEndpoint.length - 1);
+    if (urlEndpoint[urlEndpoint.length - 1] === "/") urlEndpoint = urlEndpoint.substring(0, urlEndpoint.length - 1);
     return `${urlEndpoint}/${src}?tr=${paramsString}`
 }
 
@@ -31,7 +32,8 @@ function HomeHero(props) {
             {/* Content Images */}
             <div className="flex flex-col md:flex-row">
                 <div className="w-full md:w-1/3 relative h-[55vh] group">
-                    <Image layout={"fill"} objectFit={"cover"} objectPosition={"50% 35%"} alt="Richard Doyle Illustration"
+                    <Image layout={"fill"} objectFit={"cover"} objectPosition={"50% 35%"}
+                           alt="Richard Doyle Illustration"
                            loader={imageKitLoader}
                            src="image1.jpg"
                     />
@@ -40,22 +42,42 @@ function HomeHero(props) {
                         <div className="flex justify-center w-full">
                             <div className="font-normal">
                                 <p className="text-sm">Illustration by Richard Doyle for John Ruskin’s The King of the
-                                    Golden River (from the first edition, published by Smith, Elder & Co. in 1851)</p>
+                                    Golden River (from the first edition, published by Smith, Elder & Co. in 1851).
+                                </p>
+                                <p className="text-sm">
+                                    Photo taken from {" "}
+                                    <span className="hover:text-lightBlue">
+                                    <Link href="https://catalog.princeton.edu/catalog/9939366463506421">
+                                        Princeton University Special Collections.
+                                    </Link>
+                                        </span>
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="hidden md:block w-full md:w-1/3 relative h-[55vh] group">
-                    <Image layout={"fill"} objectFit={"cover"} objectPosition={"50% 20%"} alt="Charles Ricketts & Charles Shannon Illustration"
+                    <Image layout={"fill"} objectFit={"cover"} objectPosition={"50% 20%"}
+                           alt="Charles Ricketts & Charles Shannon Illustration"
                            loader={imageKitLoader}
                            src="image2.jpg"/>
                     <div className="absolute top-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-gray-700
                      opacity-0 group-hover:opacity-100 bg-opacity-90">
                         <div className="flex justify-center w-full">
                             <div className="font-normal">
-                                <p className="text-sm">Illustration by Charles Ricketts & Charles Shannon for Oscar
+                                <p className="text-sm">
+                                    Illustration by Charles Ricketts & Charles Shannon for Oscar
                                     Wilde’s A House of Pomegranates (from the first edition, published by James R.
-                                    Osgoode McIlvaine, 1891)</p>
+                                    Osgoode McIlvaine, 1891).</p>
+                                <p className="text-sm">
+                                    Photo taken from {" "}
+                                    <span className="hover:text-lightBlue">
+
+                                    <Link href="https://catalog.princeton.edu/catalog/9924499343506421">
+                                        Princeton University Special Collections.
+                                    </Link>
+                                    </span>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -68,9 +90,18 @@ function HomeHero(props) {
                      opacity-0 group-hover:opacity-100 bg-opacity-90">
                         <div className="flex justify-center w-full">
                             <div className="font-normal">
-                                <p className="text-sm">Illustration by Walter Crane for Mary de Morgan’s The Necklace of
+                                <p className="text-sm">
+                                    Illustration by Walter Crane for Mary de Morgan’s The Necklace of
                                     Princess Fiorimonde and Other Stories (from the first edition, published by
-                                    Macmillan & Co. in 1880)</p>
+                                    Macmillan & Co. in 1880).</p>
+                                <p className="text-sm">
+                                    Photo taken from {" "}
+                                    <span className="hover:text-lightBlue">
+                                    <Link href="https://catalog.princeton.edu/catalog/9915476373506421">
+                                        Princeton University Special Collections.
+                                    </Link>
+                                    </span>
+                                </p>
                             </div>
                         </div>
                     </div>
