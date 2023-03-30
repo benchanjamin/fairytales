3797b698ada6a888673a3d2e3cedfb05b5d26f49
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2023-03-30 19:03:31 -0400

    fix nav bar

--- components/BetterHeader.tsx
+++ components/BetterHeader.tsx
@@ -4,39 +4,41 @@ import Link from "next/link";
 export default function BetterHeader() {
     return (
         <>
-            <div className="container relative mx-auto pt-6 h-[5vh] md:h-[10vh] p-6">
+            <div className="h-[5vh] p-6">
+                <div className="flex justify-between">
+                    <h2 className="text-[14px] text-white text-left font-bold font-light pl-6 pr-6 font-['Lato']">
+                        FAIRY WEB</h2>
+
+                    <button
+                        id="menu-btn"
+                        className="z-30 block md:hidden focus:outline-none hamburger"
+                    >
+                        <span className="hamburger-top"></span>
+                        <span className="hamburger-middle"></span>
+                        <span className="hamburger-bottom"></span>
+                    </button>
+                </div>
+            </div>
+            {/* Menu Items */}
+            <nav className="hidden items-center justify-between space-x-30 md:flex md:text-[24px] text-[28px] xl:text-[32px]
+                    md:mt-[15px] pl-12 pr-12">
+                <NavLink href="/" exact
+                         className="text-white italic font-medium hover:text-lightBlue duration-75 w-[100px]">Home</NavLink>
+                <NavLink href="/timeline" exact
+                         className="text-white italic font-medium hover:text-lightBlue duration-75">Timeline</NavLink>
+                <NavLink href="/map" exact
+                         className="text-white italic font-medium hover:text-lightBlue duration-75">Map</NavLink>
+                {/*<NavLink href="/settings-and-locations" exact*/}
+                {/*         className="text-white italic font-medium hover:text-lightBlue duration-75">Settings*/}
+                {/*    & Locations</NavLink>*/}
+                <NavLink href="/about" exact
+                         className="text-white italic font-medium hover:text-lightBlue duration-75">About</NavLink>
+            </nav>
+            <div className="relative mx-auto">
                 <div className="flex-col items-start">
                     {/* Logo + Button */}
-                    <div className="flex justify-between">
-                        <h2 className="text-[14px] text-white text-left font-bold font-light pl-6 pr-6 font-['Lato']">FAIRY
-                            WEB</h2>
 
-                        <button
-                            id="menu-btn"
-                            className="z-30 block md:hidden focus:outline-none hamburger"
-                        >
-                            <span className="hamburger-top"></span>
-                            <span className="hamburger-middle"></span>
-                            <span className="hamburger-bottom"></span>
-                        </button>
-                    </div>
 
-                    {/* Menu Items */}
-                    <nav className="hidden items-center justify-between space-x-30 md:flex md:text-[24px] text-[28px] xl:text-[32px]
-                    md:mt-[15px] pl-6 pr-6">
-                        <NavLink href="/" exact
-                                 className="text-white italic font-medium hover:text-lightBlue duration-75 w-[100px]">Home</NavLink>
-                        <NavLink href="/timeline" exact
-                                 className="text-white italic font-medium hover:text-lightBlue duration-75">Timeline</NavLink>
-                        <NavLink href="/map" exact
-                                 className="text-white italic font-medium hover:text-lightBlue duration-75">Map</NavLink>
-                        {/*<NavLink href="/settings-and-locations" exact*/}
-                        {/*         className="text-white italic font-medium hover:text-lightBlue duration-75">Settings*/}
-                        {/*    & Locations</NavLink>*/}
-                        <NavLink href="/about" exact
-                                 className="text-white italic font-medium hover:text-lightBlue duration-75">About</NavLink>
-
-                    </nav>
 
 
                     <div
